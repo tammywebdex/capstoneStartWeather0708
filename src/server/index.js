fc7854595126bb68659da77ef372f5d909dd8cd3
Tammy <tammy@webdex.co.uk>
2023-08-21 08:03:42 +0100

    just starting to change to geonames api

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const fetch = require('node-fetch');
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
@@ -6,6 +9,7 @@ const express = require('express');
 
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 const bodyParser = require('body-parser');
@@ -35,7 +39,24 @@ app.get('/all', sendData);
 function sendData(request, response){
     response.send(projectData);
 };
+app.get("/geonames", (req, res) => {
+    console.log("GET geonames");
+    const url = `http://api.geonames.org/searchJSON?placename=${projectData.location}&maxRows=1&username=${process.env.GEONAMES_API_ID}`;
+    console.log(url);
+    getData(url)
+        .then((response) => {
+            //  Data from Genames[0];
+            console.log(response.geonames[0]);
+            projectData.lat = response.geonames[0].lat;
+            projectData.long = response.geonames[0].lng;
 
+            console.log(`ProjectData is, ${JSON.stringify(projectData, null, 2)}`);
+            res.send(true);
+        })
+        .catch((error) => {
+            res.send(JSON.stringify({ error: error }));
+        });
+});
 //post function
 
 app.post('/add', postData);
